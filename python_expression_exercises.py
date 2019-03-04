72ace049a930d2205d668bd9e8a3fca570b5ff1c
Antti Leivo <antti.leivo@iki.fi>
2019-03-04 15:22:29 +0200

    Did exercises 1 to 3 in python_expression_exercises

---
+++ python_expression_exercises.py
@@ -0,0 +1,13 @@
+# Python Expressions Exercices (these should be practised on Python console, but this time they will be here)
+
+# Exercise 1
+var = 5280 * 23
+print(var)
+
+# Exercise 2
+var = ((10*60*60)+(35*60)+24)
+print(var)
+
+# Exercise 3
+var = (10*2)+(2*15)
+print(var)
