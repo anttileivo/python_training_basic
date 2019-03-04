40408beb6698049230ba2d0aae3040dc5ccd5c2b
Antti Leivo <antti.leivo@iki.fi>
2019-03-04 15:34:08 +0200

    Did exercises 1 to 6 and 10 in python_expression_exercises

--- python_expression_exercises.py
+++ python_expression_exercises.py
@@ -1,5 +1,9 @@
 # Python Expressions Exercices (these should be practised on Python console, but this time they will be here)
 
+# import math module
+
+import math
+
 # Exercise 1
 var = 5280 * 23
 print(var)
@@ -11,3 +15,19 @@ print(var)
 # Exercise 3
 var = (10*2)+(2*15)
 print(var)
+
+# Exercise 4
+var = (10*25)
+print(var)
+
+# Exercise 5
+var = 2*3.14*8
+print(var)
+
+# Exercise 6
+var = 3.14*8**2
+print(var)
+
+# Exercise 10
+var = math.sqrt(((2-5)**2) + ((2-6)**2))
+print( var)
