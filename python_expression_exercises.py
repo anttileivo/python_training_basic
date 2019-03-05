922b4bdb543b268e8ebd2db52b52e538d361724f
Antti Leivo <antti.leivo@iki.fi>
2019-03-05 09:55:29 +0200

    All 10 python_expression_exercises

--- python_expression_exercises.py
+++ python_expression_exercises.py
@@ -28,6 +28,28 @@ print(var)
 var = 3.14*8**2
 print(var)
 
+# Exercise 7
+var = 1000*((1+0.07)**7)
+print(var)
+
+var = 1000*(1+(0.01*7*7))
+print(var)
+
+# Exercise 8
+str1="John"
+str2="Smith"
+str3="My name is"
+str4=" "
+print(str3+str4+str1+str4+str2)
+
+# Exercise 9
+var=40
+str1="John Smith"
+str2="is"
+str3="years old"
+str4=" "
+print(str1+str4+str2+str4+str(var)+str4+str3)
+
 # Exercise 10
 var = math.sqrt(((2-5)**2) + ((2-6)**2))
-print( var)
+print(var)
